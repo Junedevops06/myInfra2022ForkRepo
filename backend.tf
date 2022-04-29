5671ab6819abe6627b0d8b7deb470d3e0f6a8c85
Junedevops06 <86455798+Junedevops06@users.noreply.github.com>
2022-04-29 15:03:24 -0500

    updated backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "my-dev2022-tf-state-bucket"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "my-2022dynamoDB-table"
   }
 }
